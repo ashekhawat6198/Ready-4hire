867632b914933c6646778ac3aa15224ea7fd4b30
Aman <jharliaman@gmail.com>
2026-03-21 22:44:15 +0530

    again url changed

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = `${import.meta.env.https://ready4hire-5di8.onrender.com/api}/users/`;
+const API_URL = `https://ready4hire-5di8.onrender.com/api/users/`;
 
 const user = JSON.parse(localStorage.getItem("user"));
 
